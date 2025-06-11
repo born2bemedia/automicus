1f9121c319df7a1f0a5d7757c38ebd4f912d834e
Daniil <dan.developer@born2be.media>
2025-06-11 18:54:30 +0300

    [feature/logo-favicon-preloader]: Logo, Favicon & Preloader (#11)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import localFont from 'next/font/local';
 
 import { cn } from '@/shared/lib/utils';
 import { Dialog } from '@/shared/ui/components/atoms';
-import { Footer, Header } from '@/shared/ui/components/organisms';
+import { Footer, Header, Preloader } from '@/shared/ui/components/organisms';
 
 import './globals.css';
 
@@ -62,6 +62,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <Dialog />
+        <Preloader />
       </body>
     </html>
   );
