e8f5096b643a102c311b4b8ea11896bf6f08573a
daniil-b2be <dan.developer@born2be.media>
2025-07-23 12:30:43 +0300

    style fixes

--- src/features/bots/ui/bot-preview.tsx
+++ src/features/bots/ui/bot-preview.tsx
@@ -6,11 +6,11 @@ import type { Bot } from '../model';
 
 export const BotPreview = ({ name, price, discount }: Bot) => {
   return (
-    <article className="border-stroke flex w-full items-center justify-between rounded-lg border p-6 max-md:flex-col max-md:items-start">
+    <article className="border-stroke flex w-full items-center justify-between gap-6 rounded-lg border p-6 max-md:flex-col max-md:items-start max-md:gap-2">
       <Text size="3xl" weight="bold" color="light">
         {name}
       </Text>
-      <div className="flex gap-2.5 max-md:flex-col">
+      <div className="flex flex-col gap-2.5 max-md:flex-col">
         <Text size="3xl" weight="bold" color="light">
           €{price.toLocaleString()}
         </Text>
