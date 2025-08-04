0309bed03856c027384d95138300b19e35e40ff2
daniil-b2be <dan.developer@born2be.media>
2025-08-04 17:36:40 +0300

    contact page

--- src/app/[locale]/contact/components/contact-us.tsx
+++ src/app/[locale]/contact/components/contact-us.tsx
@@ -1,13 +1,19 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+
 import { ContactForm } from '@/features/contact-form/ui/form';
 
 import { Title } from '@/shared/ui/components/atoms';
 
 export const ContactUs = () => {
+  const t = useTranslations('contact.contactUs');
+
   return (
     <section className="flex w-1/2 flex-col justify-center gap-10 rounded-lg bg-[#181818] p-20 max-lg:w-full max-md:p-6">
-      <Title color="light">Request Assistance</Title>
+      <Title color="light">
+        {t('title', { fallback: 'Request Assistance' })}
+      </Title>
       <ContactForm />
     </section>
   );
--- src/app/[locale]/contact/components/hero.tsx
+++ src/app/[locale]/contact/components/hero.tsx
@@ -1,17 +1,29 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
+
 import { VideoHero } from '@/shared/ui/components/organisms';
 
 import { ContactUs } from './contact-us';
 
 export const Hero = () => {
+  const t = useTranslations('contact.hero');
+
   return (
     <section className="m-1 flex gap-1 max-lg:flex-col">
       <VideoHero
-        title="We’re Here to Help. Reach Out Anytime."
-        text="Get in touch with our team for any questions, support, or assistance."
+        title={t('title', {
+          fallback: 'We’re Here to Help. Reach Out Anytime.',
+        })}
+        text={t('text', {
+          fallback:
+            'Get in touch with our team for any questions, support, or assistance.',
+        })}
         videoUrl="/videos/contact/hero.mp4"
-        metaButton={{ label: 'Check FAQ', url: '/faq' }}
+        metaButton={{
+          label: t('btn', { fallback: 'Check FAQ' }),
+          url: '/faq',
+        }}
         className="w-1/2 max-lg:h-[380px] max-lg:w-full"
         filter="rounded-lg bg-[#181818]/75"
       />
