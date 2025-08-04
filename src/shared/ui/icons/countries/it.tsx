71bb9a15fd54a4e19f992ed4c56d0265ba3e19ee
daniil-b2be <dan.developer@born2be.media>
2025-08-04 18:09:41 +0300

    json

---
+++ src/shared/ui/icons/countries/it.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export const ItIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path d="M1.5 5.20117H8.50125V18.7987H1.5V5.20117Z" fill="#009A49" />
+    <path d="M8.50098 5.20117H15.5022V18.7987H8.50098V5.20117Z" fill="white" />
+    <path d="M15.499 5.20117H22.5003V18.7987H15.499V5.20117Z" fill="#CE2B37" />
+  </svg>
+);
