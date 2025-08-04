73d70debc622ce6082fc157fe0a6a928daad70e6
daniil-b2be <dan.developer@born2be.media>
2025-08-04 17:10:47 +0300

    cart trans

--- src/features/cart/model/schemas.ts
+++ src/features/cart/model/schemas.ts
@@ -1,20 +1,17 @@
+import type { useTranslations } from 'next-intl';
+
 import { v } from '@/shared/lib/forms';
 
-export const cartSchema = v.object({
-  fullName: v.pipe(v.string(), v.minLength(3, 'This field must be filled.')),
-  email: v.pipe(v.string(), v.email('The email is invalid.')),
-  addressLine1: v.pipe(v.string(), v.minLength(1, 'This field is required.')),
-  addressLine2: v.pipe(v.string(), v.minLength(1, 'This field is required.')),
-  city: v.pipe(v.string(), v.minLength(1, 'This field is required.')),
-  country: v.pipe(v.string(), v.minLength(1, 'This field is required.')),
-  zip: v.pipe(v.string(), v.minLength(1, 'This field is required.')),
-  phone: v.pipe(v.string(), v.minLength(5, 'This field is required.')),
-  isAgreeRefund: v.pipe(
-    v.boolean(),
-    v.literal(true, 'You must agree to the terms and conditions'),
-  ),
-  isAgreeTerms: v.pipe(
-    v.boolean(),
-    v.literal(true, 'You must agree to the terms and conditions'),
-  ),
-});
+export const getCartSchema = (t: ReturnType<typeof useTranslations>) =>
+  v.object({
+    fullName: v.pipe(v.string(), v.minLength(3, t('mustFilled'))),
+    email: v.pipe(v.string(), v.email(t('email'))),
+    addressLine1: v.pipe(v.string(), v.minLength(1, t('required'))),
+    addressLine2: v.pipe(v.string(), v.minLength(1, t('required'))),
+    city: v.pipe(v.string(), v.minLength(1, t('required'))),
+    country: v.pipe(v.string(), v.minLength(1, t('required'))),
+    zip: v.pipe(v.string(), v.minLength(1, t('required'))),
+    phone: v.pipe(v.string(), v.minLength(5, t('required'))),
+    isAgreeRefund: v.pipe(v.boolean(), v.literal(true, t('required'))),
+    isAgreeTerms: v.pipe(v.boolean(), v.literal(true, t('required'))),
+  });
